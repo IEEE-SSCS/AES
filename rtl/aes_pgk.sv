90c1df71489e3b1d43d15e68cbe5604908171a2c
Moustafa Mahmoud <e.moustafa2@gmail.com>
2021-04-18 23:20:15 +0200

    aes_enc and shift_rows implemented

--- rtl/aes_pgk.sv
+++ rtl/aes_pgk.sv
@@ -6,6 +6,7 @@ package aes_pkg;
     typedef logic [0:23][7:0] aes_192;
     typedef logic [0:31][7:0] aes_256;
     typedef logic [0:3][7:0]  aes_32;
+    typedef logic [0:3][31:0] key_128;
 
     typedef logic [7:0] aes_byte;
     typedef logic [3:0] aes_nibble;
@@ -145,5 +146,26 @@ package aes_pkg;
         out[7] = in[1] ^ in[4] ^ in[6];
         return out;
     endfunction : inv_affine
+    
+    function automatic aes_128 shift_rows(aes_128 in);
+      aes_128 out;
+      out[0]  = in[0];
+      out[1]  = in[5];
+      out[2]  = in[10];
+      out[3]  = in[15];
+      out[4]  = in[4];
+      out[5]  = in[9];
+      out[6]  = in[14];
+      out[7]  = in[3];
+      out[8]  = in[8];
+      out[9]  = in[13];
+      out[10] = in[2];
+      out[11] = in[7];
+      out[12] = in[12];
+      out[13] = in[1];
+      out[14] = in[6];
+      out[15] = in[11]; 
+      return out;
+    endfunction : shift_rows
 
 endpackage : aes_pkg
