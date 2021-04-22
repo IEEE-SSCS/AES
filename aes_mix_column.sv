f9e83c7d2f62f01e4d90d2cf24ad412bbb2e82bf
Yomna-AlShaboury <60438598+Yomna-AlShaboury@users.noreply.github.com>
2021-04-22 22:50:21 +0200

    Delete aes_mix_column.sv

--- aes_mix_column.sv
+++
@@ -1,9 +0,0 @@
-import aes_pkg::*;
-module aes_mix_column(
-	input aes_pkg::aes_128 state_i,
-	output aes_pkg::aes_128 state_o);
-for (genvar i = 0; i < 4; i++) begin
-        aes_mix_single_column Ci(.state_i(state_i[4*i:(4*(i+1))-1]), .state_o(state_o[4*i:(4*(i+1))-1]));
-    end
-
-endmodule
