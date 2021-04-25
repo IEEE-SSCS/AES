b964293cb9f904b38309af4f5eb16c2cdf197a15
Mohamed Abdalla Mohamed <72136113+MohamdAbdalla@users.noreply.github.com>
2021-04-25 16:26:58 +0200

    minor changes

--- rtl/aes_key_gen.sv
+++ rtl/aes_key_gen.sv
@@ -1,5 +1,4 @@
-module aes_key_gen
- (
+module aes_key_gen (
 
 input logic clk,nrst ,en ,gen_key,next_rnd,
 input  aes_pkg:: aes_byte r_con_ctrl,r_con_i,
@@ -17,23 +16,22 @@ aes_pkg:: aes_byte rcon_o;
 aes_pkg:: aes_32 xor_result; 
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////
 //Muxs
-//first mux so we will use it's out
+//first mux 
 assign rcon_o  = gen_key ? r_con_ctrl : r_con_i;
-//secound mux so we will use it's out
+//secound mux 
 assign key_round  = next_rnd ? key_o : key_i;
+////////////////////////////////////////////////////////////////////////////////////////
 
-assign word_rnd_in=key_round;
-
-assign Sub_o =( word_rnd_in[3] << 8); // sub byte from s box 
-
+assign word_rnd_in = key_round;
+assign Sub_o = ( word_rnd_in[3] << 8); //  to s_box 
+assign xor_result = 32'({(rcon_o^Sub_i[0]),Sub_i[1:3]});
 
 
 always_comb
 begin
-xor_result=32'({rcon_o^Sub_i[0],Sub_i[1:3]});
-word_rnd_out[0] = word_rnd_in[0] ^ (xor_result);//rcon from ctrl&sub_i from s_box
+word_rnd_out[0] = word_rnd_in[0] ^ (xor_result);//rcon from ctrl^sub_i from s_box
 word_rnd_out[1] = word_rnd_in[1] ^ word_rnd_out[0];
 word_rnd_out[2] = word_rnd_in[2] ^ word_rnd_out[1];
 word_rnd_out[3] = word_rnd_in[3] ^ word_rnd_out[2];
