602b7e0af5060e9d619aa54d1794a0de91ab7539
Omar Younis <56393026+OYounis@users.noreply.github.com>
2021-05-03 02:55:54 +0200

    Update unittest/aes_mix_column.sv
    unittest/aes_mix_column.sv
    
    * Name change to aes_mix_column_tb.sv
    * Minor fixes.
    
    Co-Authored-By: HasnaaMahmoud <81872582+HasnaaMahmoud@users.noreply.github.com>

---
+++ unittests/aes_mix_column_tb.sv
@@ -0,0 +1,36 @@
+module aes_mix_column_tb;
+    timeunit 1ns;   //systemverilog equivalent for `timescale
+    
+    logic [0:15][7:0] state_i;
+    logic [0:15][7:0] state_o;
+    
+    
+    byte A[16], B[16];
+
+    import "DPI-C" function void mix_single_column(input byte in[4],  output byte out[4]);
+
+    aes_mix_column c1(
+        .state_i(state_i),
+        .state_o(state_o)
+    );
+
+
+  initial begin
+	repeat(1000) begin  //generate 1000 stimuli
+            foreach (A[i]) begin
+                A[i] = $urandom();
+                state_i[i] = A[i]; 
+            end
+
+            mix_single_column(A[0:3],   B[0:3]);
+            mix_single_column(A[4:7],   B[4:7]);
+            mix_single_column(A[8:11],  B[8:11]);
+            mix_single_column(A[12:15], B[12:15]);
+
+            #1 foreach (B[i]) begin
+                check: assert(B[i] == state_o[i])   //check if refrence is equivalent to DUT
+                    else $fatal("check failed: byte index = %d, A = %h, B = %h, state_o = %h", i, A[i], B[i], state_o[i]); //if not generate fatal error
+               end
+ 	end
+  end
+endmodule 
