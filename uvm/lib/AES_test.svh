a80797727d6a41096381327c6d4fae14d2383143
marwan ibrahim zaki <50636605+marwanibrahim546@users.noreply.github.com>
2021-09-23 20:16:12 +0200

    edited

---
+++ uvm/lib/AES_test.svh
@@ -0,0 +1,24 @@
+class AES_test extends uvm_test;
+    `uvm_component_utils(AES_test)
+
+    AES_env aes_env;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction: new
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        aes_env = AES_env::type_id::create(.name("aes_env"), .parent(this));
+    endfunction: build_phase
+
+    task run_phase(uvm_phase phase);
+        AES_sequence aes_seq;
+
+        phase.raise_objection(.obj(this));
+            aes_seq = simpleadder_sequence::type_id::create(.name("sa_seq"), .contxt(get_full_name()));
+            assert(aes_seq.randomize());
+            aes_seq.start(aes_env.sa_agent.sa_seqr);
+        phase.drop_objection(.obj(this));
+    endtask: run_phase
+endclass: AES_test
