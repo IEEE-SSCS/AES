2133e1b98642aff8d671565d6152bee630f614d5
NermeenMohammed-M <89461947+NermeenMohammed-M@users.noreply.github.com>
2021-10-12 22:12:47 +0200

    Update and rename AES_env.svh to aes_env.svh

---
+++ uvm/lib/aes_env.svh
@@ -0,0 +1,22 @@
+class aes_env extends uvm_env;
+	`uvm_component_utils(aes_env)
+
+	aes_agent agent;
+	aes_scoreboard aes_sb;
+
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction: new
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		agent	= aes_agent::type_id::create(.name("agent"), .parent(this));
+		aes_sb		= aes_scoreboard::type_id::create(.name("aes_sb"), .parent(this));
+	endfunction: build_phase
+
+	function void connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+		agent.agent_ap_before.connect(aes_sb.sb_export_before);
+		agent.agent_ap_after.connect(aes_sb.sb_export_after);
+	endfunction: connect_phase
+endclass: aes_env 
