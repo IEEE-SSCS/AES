a82ffd512bd511d7038cc40ad2bdf2f0d50fd2be
NermeenMohammed-M <89461947+NermeenMohammed-M@users.noreply.github.com>
2021-10-12 22:20:03 +0200

    Update pkg_main.sv

--- uvm/lib/pkg_main.sv
+++ uvm/lib/pkg_main.sv
@@ -8,14 +8,19 @@ import aes_pkg::*;
 	`include "aes_transaction.sv"
 	`include "aes_sequence.sv"
 	`include "aes_sequencer.sv"
-	//`include "aes_monitor_before.sv"
+	`include "aes_monitor.sv"
 	//`include "aes_monitor_after.sv"
 	`include "aes_driver.sv"
 	//`include "aes_agent_config.sv"
-	//`include "aes_agent.sv"
-	//`include "aes_scoreboard.sv"
+	`include "aes_agent.sv"
+	`include "fill.sv"
+
+	
+	`include "aes_scoreboard.sv"
 	//`include "aes_env_config.sv"
-	//`include "aes_env.sv"
-	//`include "aes_test.sv"
+	`include "aes_env.sv"
+	`include "aes_test.sv"
+        
+	
 
 endpackage: pkg_main
